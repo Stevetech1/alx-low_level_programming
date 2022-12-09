8e2d2bb173a1e963fc380a98081bd4183a05e8d4
Stevetech1 <steveojata54@gmail.com>
2022-12-09 12:26:08 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,10 @@
                                                            
  * main - Entry
                                                  
- * Return: Always 0                                               
+ * Return: Always 0
+ 
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
