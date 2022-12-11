a1732647a9e39f3260e73462b1d8f81c6f5b8fc3
Stevetech1 <steveojata54@gmail.com>
2022-12-11 05:31:58 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - print alpha in lower case
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char lu;
+for (lu = 'a'; lc <= 'z'; lc++)
+	{
+	putchar(lu)
+	}
+putchar('\n');
+return (0);
+}
