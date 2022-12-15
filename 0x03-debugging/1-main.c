767c438cb49f6b5cb99def53cf6517d08bafe2c0
Stevetech1 <steveojata54@gmail.com>
2022-12-14 16:07:47 -0800

    Debuggjng 2

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - cuases an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+
+	/**
+	 * white (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
