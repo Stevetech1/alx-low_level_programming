14dd6bfe7e51123b636286d99d65492ebcd63192
Stevetech1 <steveojata54@gmail.com>
2022-12-18 15:37:57 -0800

    print triangle

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * main - header file for prototypes
- * void prototypes (void)
- * int prototypes (int)
- */
 int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
@@ -16,7 +11,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-void print_traingle(int size);
+void print_triangle(int size);
 void print_number(int n);
 
-#endif /* MAIN_H */
+#endif
