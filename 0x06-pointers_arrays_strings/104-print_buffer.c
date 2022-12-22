aaf98310fa57231971d467bbc2dc053f05d02024
Stevetech1 <steveojata54@gmail.com>
2022-12-22 08:46:04 -0800

    Advanced 5

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,5 +1,6 @@
 #include "main.h"
-#include <stdio.h>
+
+
 /**
  * print_buffer - prints a buffer
  *
@@ -9,7 +10,7 @@
  */
 void print_buffer(char *b, int size)
 {
-	int j, k, l;
+int j, k, l;
 
 	if (size <= 0)
 		printf("\n");
