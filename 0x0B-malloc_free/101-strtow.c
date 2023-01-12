879868104ca9acd3e8b44c808c37d8c82b980217
Stevetech1 <steveojata54@gmail.com>
2023-01-11 18:53:22 -0800

    Write a function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,41 +1,97 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * *argstostr - main entry
- * @ac: int input
- * @av: double pointer array
- *
- * Return: 0
+ * argstostr - concatenates all the arguments
+ * @ac: argument count
+ * @av: arguments passed
+ * Return: pointer to a string or NULL
  */
 char *argstostr(int ac, char **av)
 {
-	int i, n, r = 0, l = 0;
-	char *str;
+	char *mem;
+	unsigned int total;
 
 	if (ac == 0 || av == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
 	{
-		for (n = 0; av[i][n]; n++)
-			l++;
-	}
-	l += ac;
-	str = malloc(sizeof(char) * i + 1);
-	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-		for (n = 0; av[i][n]; n++)
-		{
-			str[r] = av[i][n];
-			r++;
-		}
-		if (str[r] == '\0')
-		{
-			str[r++] = '\n';
-		}
 	}
-	return (str);
+	total = total_length(ac, av);
+	mem = malloc(total * sizeof(char));
+	copy_string(av, mem);
+	return (mem);
+}
+/**
+ * _strlen - getting str length of every argv
+ * @s: the string
+ * Return: str len
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * total_length - total length of all argv
+ * @ac: counts argv
+ * @av: arguments
+ * Return: sum
+ */
+unsigned int total_length(int ac, char **av)
+{
+	int i;
+	unsigned int sum;
+
+	i = 0;
+	sum = 0;
+	while (av[i] != NULL)
+	{
+		sum = sum + _strlen(av[i]);
+		i++;
+	}
+	sum = sum + ac + 1;
+	return (sum);
+}
+/**
+ * copy_string - concat strings
+ * @av: arguments
+ * @s: string
+ * Return: str values
+ */
+void copy_string(char **av, char *s)
+{
+	int i;
+	int k;
+
+	i = 0;
+	k = 0;
+	while (av[i] != NULL)
+	{
+		from_to(av[i], s + k);
+		k = k + _strlen(av[i]);
+		s[k] = '\n';
+		k++;
+		i++;
+	}
+	s[k] = '\0';
+}
+/**
+ * from_to - copies values
+ * @origin: copy from
+ * @dest: copy to
+ * Return: nothing
+ */
+void from_to(char *origin, char *dest)
+{
+	int i;
+
+	i = 0;
+	while (origin[i] != '\0')
+	{
+		dest[i] = origin[i];
+		i++;
+	}
 }
